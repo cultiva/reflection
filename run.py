5b6c85ae03a0ce771198f3b915e092d1a9f977c4
kosakuhida <kosakuhida@kosakuhidanoMacBook-puro.local>
2018-12-29 13:27:00 +0800

    run.pyにお試しで追加プラグインを入力。I love you.

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
 from slackbot.bot import Bot
 
+# プラグイン追加用に読み込み
+# リファレンスはhttps://github.com/lins05/slackbot#create-plugins
+
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+import re
+
 def main():
     bot = Bot()
     bot.run()
 
 if __name__ == "__main__":
     main()
+
+# ここから追加プラグイン
+@respond_to('I love you')
+def love(message):
+    message.reply('I love you too!')
